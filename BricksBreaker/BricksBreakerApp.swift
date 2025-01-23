08b739467d494d73bbedd5f45ef690b621ecaaf3
keshew <artyom.korotckov2017@yandex.ru>
2025-01-24 00:08:21 +0500

    Initial Commit

---
+++ BricksBreaker/BricksBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BricksBreakerApp.swift
+//  BricksBreaker
+//
+//  Created by Артём Коротков on 24.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct BricksBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
